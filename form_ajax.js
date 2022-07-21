ddb6f290bb48b01a29a568ae1e461dda33b5fa7b
Kotall1 <kvas.ahabanin@gmail.com>
2022-07-21 21:51:05 +0300

    Hw compeleted

---
+++ form_ajax.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    var formData = {
+      user_name: $("#user_name").val(),
+      user_second_name: $("#user_second_name").val(),
+      user_last_name: $("#user_last_name").val(),
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "dadata.php",
+      data: formData,
+      dataType: "json",
+      encode: true,
+    }).done(function (result) {
+      console.log('Array result', result);
+
+    for (let value of result) {
+
+        $("#result").html(
+            '<p>Полное имя ' + value.result + '</p>' +
+            '<p>В лице ' + value.result_genitive + '</p>' +
+            '<p>Кому ' + value.result_dative + '</p>' +
+            '<p>Согласовано c ' + value.result_ablative + '</p>' +
+            '<hr>' +
+            '<p>Имя: ' + value.name + '</p>' +
+            '<p>Отчество: ' + value.patronymic + '</p>' +
+            '<p>Фамилия: ' + value.surname + '</p>'
+
+          );
+      console.log('Array value', value);
+    }
+
+    });
+
+    event.preventDefault();
+  });
+});
